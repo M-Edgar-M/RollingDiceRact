e110a2466b4e26fba9363d5cd931eb0e08c661aa
Edgar <edgar.mkhitaryan989@gmail.com>
2021-07-23 22:09:56 +0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -4,20 +4,45 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      
+      <div id="logo">
+      <img
+        src="/img/logo-dice.png"
+        alt="Pigy"
+      />
+      <h1>Rolling Dice</h1>
+    </div>
+    <div id="content-wrapper">
+      <div id="left_side">
+        <h1>Player 1</h1>
+        <div class="current_score current_score_0">22</div>
+        <button class="btn btn_roll_0">Button Roll</button>
+        <button class="btn btn_hold">Button Hold</button>
+      </div>
+      <div id="mid_content">
+        <button class="btn btn_newgame">New Game</button>
+        <img src="img/dice-1.png" alt="Dice" class="dice_img" />
+        <p>All Rights Reserved &copy;</p>
+      </div>
+
+      <div id="right_side">
+        <h1>Player 2</h1>
+        <div class="current_score current_score_1">55</div>
+        <button class="btn btn_roll_1">Button Roll</button>
+        <button class="btn btn_hold">Button Hold</button>
+      </div>
+    </div>
+
+    <footer>
+      <div class="footer">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Player Rolls the Dice, if it's not 2, the game continuous and the
+          Player can Hold the Score untill he gets 101. If Dice is 1 then Player
+          loses his progress and turn is passed to the other Player.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
+    </footer>
+
     </div>
   );
 }
