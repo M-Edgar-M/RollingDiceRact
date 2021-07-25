999153caa611ff3bf869a5ffc016123dc59e9d39
Edgar <edgar.mkhitaryan989@gmail.com>
2021-07-25 22:18:37 +0400

    Update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-import './App.css';
-import { useRef, useState } from 'react';
+import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
-
   let [currentScore, setCurrentScore] = useState(0);
   let [currentPlayer, setCurrentPlayer] = useState(0);
   let [firstPlayerScore, setFirstPlayerScore] = useState(0);
@@ -12,89 +11,98 @@ function App() {
   // const playerSecondBtn = useRef(false);
   let dice;
 
+  const btnRoll = () => {
+    dice = Math.floor(Math.random() * 6) + 1;
 
-    const btnRoll = () => {
-        dice = Math.floor(Math.random() * 6) + 1;
-      
-      if(dice !== 2) {
-       setCurrentScore(currentScore += dice);
-       
-      } else {
-        if(currentPlayer === 0) {
-          setCurrentPlayer(1);
-        } else if(currentPlayer === 1) {
-          setCurrentPlayer(0);
-
-        }
-          setCurrentScore(0);
+    if (dice !== 2) {
+      setCurrentScore((currentScore += dice));
+    } else {
+      if (currentPlayer === 0) {
+        setCurrentPlayer(1);
+      } else if (currentPlayer === 1) {
+        setCurrentPlayer(0);
       }
-    };
+      setCurrentScore(0);
+      setDisable(!disable);
+    }
+  };
 
-    const btnHold = () => {
-      if(currentPlayer === 1) {
-        setCurrentScore(0);
-        setFirstPlayerScore(firstPlayerScore += currentScore);
-        setCurrentPlayer(1)
+  const btnHold = () => {
+    if (currentPlayer === 0) {
+      setCurrentScore(0);
+      setFirstPlayerScore((firstPlayerScore += currentScore));
+      setCurrentPlayer(1);
+      setDisable(!disable);
+    } else {
+      setSecondPlayerScore((secondPlayerScore += currentScore));
+      setCurrentScore(0);
+      setCurrentPlayer(0);
+      setDisable(!disable);
+    }
+  };
 
-      } else {
-        setSecondPlayerScore(secondPlayerScore += currentScore);
-        setCurrentScore(0);
-        setCurrentPlayer(0)
-
-      }
-      
-    };
-
-
-
-    console.log("Current Score is " + currentScore);
-    console.log("Current Player is  " + currentPlayer);
-    console.log(firstPlayerScore);
-    console.log(secondPlayerScore);
+  useEffect(() => {
     
+    
+  },firstPlayerScore, secondPlayerScore)
 
+  // console.log("Current Score is " + currentScore);
+  // console.log("Current Player is  " + currentPlayer);
+  // console.log("First Player is " + firstPlayerScore);
+  // console.log("Second Player is " + secondPlayerScore);
 
   return (
     <div className="App">
-      
       <div id="logo">
-      <img
-        src="/img/logoDice.svg"
-        alt="Pigy"
-      />
-      <h1>Rolling Dice</h1>
-    </div>
-    <div id="content-wrapper">
-      <div id="left_side">
-        <h1>Player 1</h1>
-        <div className="current_score current_score_0">55</div>
-        <button className="btn btn_roll_0" onClick={btnRoll} disabled={disable}>Button Roll</button>
-        <button className="btn btn_hold"onClick={btnHold} disabled={disable}>Button Hold</button>
+        <img src="/img/logoDice.svg" alt="Pigy" />
+        <h1>Rolling Dice</h1>
       </div>
-      <div id="mid_content">
-        <button className="btn btn_newgame">New Game</button>
-        <img src='img/dice-4.png' alt="Dice" className="dice_img" />
-        <p>All Rights Reserved &copy;</p>
+      <div id="content-wrapper">
+        <div id="left_side">
+          <h1>Player 1</h1>
+          <div className="current_score current_score_0">{firstPlayerScore}</div>
+          <button
+            className="btn btn_roll_0"
+            onClick={btnRoll}
+            disabled={disable}
+          >
+            Button Roll
+          </button>
+          <button className="btn btn_hold" onClick={btnHold} disabled={disable}>
+            Button Hold
+          </button>
+        </div>
+        <div id="mid_content">
+          <button className="btn btn_newgame">New Game</button>
+          <img src="img/dice-4.png" alt="Dice" className="dice_img" />
+          <p>All Rights Reserved &copy;</p>
+        </div>
+
+        <div id="right_side">
+          <h1>Player 2</h1>
+          <div className="current_score current_score_1">{secondPlayerScore}</div>
+          <button
+            className="btn btn_roll_1"
+            onClick={btnRoll}
+            disabled={!disable}
+          >
+            Button Roll
+          </button>
+          <button className="btn btn_hold" onClick={btnHold} disabled={!disable}>
+            Button Hold
+          </button>
+        </div>
       </div>
 
-      <div id="right_side">
-        <h1>Player 2</h1>
-        <div className="current_score current_score_1">0</div>
-        <button className="btn btn_roll_1" onClick={btnRoll} disabled={disable}>Button Roll</button>
-        <button className="btn btn_hold" onClick={btnHold} disabled={disable}>Button Hold</button>
-      </div>
-    </div>
-
-    <footer>
-      <div className="footer">
-        <p>
-          Player Rolls the Dice, if it's not 2, the game continuous and the
-          Player can Hold the Score untill he gets 101. If Dice is 1 then Player
-          loses his progress and turn is passed to the other Player.
-        </p>
-      </div>
-    </footer>
-
+      <footer>
+        <div className="footer">
+          <p>
+            Player Rolls the Dice, if it's not 2, the game continuous and the
+            Player can Hold the Score untill he gets 101. If Dice is 1 then
+            Player loses his progress and turn is passed to the other Player.
+          </p>
+        </div>
+      </footer>
     </div>
   );
 }
